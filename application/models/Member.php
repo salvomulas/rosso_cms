dc2abc973b877a24f8f8fce2d4cbed80f46b8429
Salvatore Mulas <salvo@salvoblog.ch>
2013-07-17 01:14:18 +0200

    Member model initiated

---
+++ application/models/Member.php
@@ -0,0 +1,19 @@
+<?php
+
+class Member extends CI_Model {
+
+    private $firstName;
+    private $lastName;
+    private $type;
+
+    private static $totCounter;
+    private static $actCounter;
+    private static $psvCounter;
+
+    public function __construct() {
+
+    }
+
+
+
+}

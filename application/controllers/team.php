3299b0c7cda1192f2a867ca059b2ade7b0d63723
Salvatore Mulas <salvo@salvoblog.ch>
2013-08-08 01:23:40 +0200

    Adjusted Player and Team model. Minor changes to the views

--- application/controllers/team.php
+++ application/controllers/team.php
@@ -9,6 +9,21 @@ class Team extends MY_Controller {
         $this->aktive(1);
     }
 
+    public function sandbox() {
+        $this->load->model('teams');
+        $this->load->model('player');
+
+        $data['teamName'] = $this->teams->getTeamName(1);
+        $data['spieler'] = $this->player->getPlayers(1);
+        $data ['page_title'] = "Sandbox Mannschaften";
+
+        $this->load->view("meta/metadata", $data);
+        $this->load->view("elements/topNav");
+        $this->load->view("elements/teamUnit", $data);
+        $this->load->view("pages/team");
+        $this->load->view("elements/footer");
+        }
+
     public function aktive($teamID) {
         $this->load->model('teams');
         $this->load->model('player');
@@ -17,6 +32,7 @@ class Team extends MY_Controller {
         $data['spieler'] = $this->player->getPlayers($teamID);
 
         echo $data['teamName'];
+
         foreach ($data['spieler'] as $row) {
             echo $row->lastName;
             echo $row->firstName;
---
+++ application/views/elements/teamUnit.php
@@ -0,0 +1,14 @@
+<div class="hero-unit">
+    <div class="container">
+        <div class="row">
+            <div class="span8">
+                <h3>Sandbox Mannschaft</h3>
+            </div>
+            <div class="span4">
+                <div class="well-small">
+                    <h4>Musterinhalt</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>

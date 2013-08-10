a47db9fcb35e63232ec72ccd8d627b4d049c97d6
Salvatore Mulas <salvo@salvoblog.ch>
2013-08-10 16:17:53 +0200

    Minor MVC adjustments

--- application/views/elements/playerTable.php
+++ application/views/elements/playerTable.php
@@ -1,8 +1,9 @@
 <table class="table table-hover table-condensed">
     <thead>
         <tr>
+            <th>Nr.</th>
             <th>Nachname</th>
-            <th>Vornamename</th>
+            <th>Vorname</th>
             <th>Position</th>
             <th>Geb. Datum</th>
         </tr>
@@ -11,9 +12,20 @@
         <?php
         foreach ($spieler as $row) {
             echo '<tr>';
+            echo '<td>' . $row->trikot . '</td>';
             echo '<td>' . $row->lastName . '</td>';
             echo '<td>' . $row->firstName . '</td>';
-            echo '<td></td>';
+            switch ($row->toPosition) {
+                case 1: echo '<td>Torwart</td>';
+                break;
+                case 2: echo '<td>Verteidiger</td>';
+                break;
+                case 3: echo '<td>Mittelfeldspieler</td>';
+                break;
+                case 4: echo '<td>St&uuml;rmer</td>';
+                break;
+                default: echo '<td></td>';
+            }
             echo '<td>' . $row->bDay . '</td>';
             echo '</tr>';
         }
--- application/views/pages/team.php
+++ application/views/pages/team.php
@@ -58,9 +58,107 @@
                 <div class="tab-content">
                     <div class="tab-pane active" id="tab1">
 
+                        <h4>Torh&uuml;ter</h4>
+                        <hr>
                         <div class="row-fluid">
-
+                            <div class="span4 text-center">
+                                <img class="img-rounded" src="<?php echo base_url(); ?>assets/img/member/00100.jpg" style="height: 150px; width: 150px;">
+                                <h4>1</h4><h5>Stefan Gubser</h5>
+                            </div>
+                            <div class="span4 text-center">
+                                <img class="img-rounded" src="<?php echo base_url(); ?>assets/img/member/00101.jpg" style="height: 150px; width: 150px;">
+                                <h4>12</h4><h5>Gaetano Ciraolo</h5>
+                            </div>
                         </div>
+
+                        <h4>Verteidigung</h4>
+                        <hr>
+                        <div class="row-fluid">
+                            <div class="span4 text-center">
+                                <img class="img-rounded" src="<?php echo base_url(); ?>assets/img/dummy_person.jpg" style="height: 150px; width: 150px;">
+                                <h4>3</h4><h5>Luca Lo Presti</h5>
+                            </div>
+                            <div class="span4 text-center">
+                                <img class="img-rounded" src="<?php echo base_url(); ?>assets/img/member/00102.jpg" style="height: 150px; width: 150px;">
+                                <h4>5</h4><h5>Andrea Mulas</h5>
+                            </div>
+                            <div class="span4 text-center">
+                                <img class="img-rounded" src="<?php echo base_url(); ?>assets/img/member/00103.jpg" style="height: 150px; width: 150px;">
+                                <p><span class="label label-important">Captain</span> 6</p><h5>Fabrizio Santoni</h5>
+                            </div>
+                        </div>
+                        <p></p>
+                        <div class="row-fluid">
+                            <div class="span4 text-center">
+                                <img class="img-rounded" src="<?php echo base_url(); ?>assets/img/member/00104.jpg" style="height: 150px; width: 150px;">
+                                <h4>8</h4><h5>Sebastian Werisch</h5>
+                            </div>
+                            <div class="span4 text-center">
+                                <img class="img-rounded" src="<?php echo base_url(); ?>assets/img/member/00105.jpg" style="height: 150px; width: 150px;">
+                                <h4>13</h4><h5>Stefano Pilia</h5>
+                            </div>
+                            <div class="span4 text-center">
+                                <img class="img-rounded" src="<?php echo base_url(); ?>assets/img/member/00106.jpg" style="height: 150px; width: 150px;">
+                                <h4>15</h4><h5>Lukas Rickenbacher</h5>
+                            </div>
+                        </div>
+                        <p></p>
+                        <div class="row-fluid">
+                            <div class="span4 text-center">
+                                <img class="img-rounded" src="<?php echo base_url(); ?>assets/img/dummy_person.jpg" style="height: 150px; width: 150px;">
+                                <h4>21</h4><h5>Alessandro Gatti</h5>
+                            </div>
+                        </div>
+                        <hr>
+
+                        <h4>Mittelfeld</h4>
+                        <hr>
+                        <div class="row-fluid">
+                            <div class="span4 text-center">
+                                <img class="img-rounded" src="<?php echo base_url(); ?>assets/img/member/00107.jpg" style="height: 150px; width: 150px;">
+                                <h4>7</h4><h5>Flavio R&uuml;edi</h5>
+                            </div>
+                            <div class="span4 text-center">
+                                <img class="img-rounded" src="<?php echo base_url(); ?>assets/img/member/00108.jpg" style="height: 150px; width: 150px;">
+                                <h4>9</h4><h5>Danilo Cordeiro</h5>
+                            </div>
+                            <div class="span4 text-center">
+                                <img class="img-rounded" src="<?php echo base_url(); ?>assets/img/member/00109.jpg" style="height: 150px; width: 150px;">
+                                <h4>10</h4><h5>Davide Branca</h5>
+                            </div>
+                        </div>
+                        <p></p>
+                        <div class="row-fluid">
+                            <div class="span4 text-center">
+                                <img class="img-rounded" src="<?php echo base_url(); ?>assets/img/dummy_person.jpg" style="height: 150px; width: 150px;">
+                                <h4><11</h4><h5>David Tettey</h5>
+                            </div>
+                            <div class="span4 text-center">
+                                <img class="img-rounded" src="<?php echo base_url(); ?>assets/img/dummy_person.jpg" style="height: 150px; width: 150px;">
+                                <h4>14</h4><h5>Berat Argun</h5>
+                            </div>
+                            <div class="span4 text-center">
+                                <img class="img-rounded" src="<?php echo base_url(); ?>assets/img/member/00110.jpg" style="height: 150px; width: 150px;">
+                                <h4>16</h4><h5>Domenico Chiriaco</h5>
+                            </div>
+                        </div>
+                        <p></p>
+                        <div class="row-fluid">
+                            <div class="span4 text-center">
+                                <img class="img-rounded" src="<?php echo base_url(); ?>assets/img/member/00111.jpg" style="height: 150px; width: 150px;">
+                                <h4>17</h4><h5>Denis Sahin</h5>
+                            </div>
+                            <div class="span4 text-center">
+                                <img class="img-rounded" src="<?php echo base_url(); ?>assets/img/member/00112.jpg" style="height: 150px; width: 150px;">
+                                <h4>18</h4><h5>Salvatore Mulas</h5>
+                            </div>
+                            <div class="span4 text-center">
+                                <img class="img-rounded" src="<?php echo base_url(); ?>assets/img/member/00113.jpg" style="height: 150px; width: 150px;">
+                                <h4>20</h4><h5>Alessandro Branca</h5>
+                            </div>
+                        </div>
+                        <hr>
+
 
                     </div>
                     <div class="tab-pane" id="tab2">

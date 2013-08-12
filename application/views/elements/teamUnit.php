8b9fc60158eb16adbe2c136fae0efc69c894ac91
Salvatore Mulas <salvo@salvoblog.ch>
2013-08-12 21:35:26 +0200

    New view created for Team overview

--- application/views/elements/teamUnit.php
+++ application/views/elements/teamUnit.php
@@ -1,13 +1,18 @@
 <div class="hero-unit">
     <div class="container">
         <div class="row">
-            <div class="span8">
-                <h3>Sandbox Mannschaft</h3>
-                <p>4. Liga - Gruppe 2</p>
+            <div class="span8 pull-right">
+                <img src="<?php echo base_url(); ?>assets/img/mannschaft.jpg">
             </div>
             <div class="span4">
-                <div class="well-hero">
-                </div>
+                <h3>Erste Mannschaft</h3>
+                <p>4. Liga - Gruppe 2</p>
+                <ul class="nav nav-tabs nav-stacked">
+                    <li class="active"><a href="#tab1" data-toggle="tab">Kader</a></li>
+                    <li><a href="#tab2" data-toggle="tab">Spielerliste</a></li>
+                    <li><a href="#tab3" data-toggle="tab">Spielkalender</a></li>
+                    <li><a href="#tab4" data-toggle="tab">Matchberichte</a></li>
+                </ul>
             </div>
         </div>
     </div>

e125a24076fde70b42647d52f505148d6bc873c2
Salvatore Mulas <salvo@salvoblog.ch>
2013-08-31 15:23:05 +0200

    Initiated work on the formation library

--- application/controllers/sandbox.php
+++ application/controllers/sandbox.php
@@ -7,12 +7,49 @@ class Sandbox extends CI_Controller {
 
 	public function index() {
 
-		$array = array (1,2,3,4,5);
+        // Aufstellung zum Testen hier eingeben
+        $aufstellung = array (4,4,1,1);
 
-        var_dump($array);
+        // Berechne die Array-Grösse
+        $anzahl = sizeof($aufstellung);
 
-        echo serialize($array);
+        // Variableninitialisierung für Gültigkeit
+        $validity = false;
 
-	}
+        // Gültigkeitsrechnung falls nicht 10 Spieler
+        if (array_sum($aufstellung) == 10) {
+            $validity = true;
+        } else {
+            $validity = false;
+        }
+
+        // Wenn gültig fahre fort
+        if ($validity) {
+
+            switch ($anzahl) {
+
+                case 3:
+                    echo "Verteidiger: ".$aufstellung[0]."<br />";
+                    echo "Mittelfeld: ".$aufstellung[1]."<br />";
+                    echo "Sturm: ".$aufstellung[2]."<br />";
+                break;
+                case 4:
+                    echo "Verteidiger: ".$aufstellung[0]."<br />";
+                    echo "Defensiv Mittelfeld: ".$aufstellung[1]."<br />";
+                    echo "Offensiv Mittelfeld: ".$aufstellung[2]."<br />";
+                    echo "Sturm: ".$aufstellung[3]."<br />";
+                break;
+                default:
+                    echo "Ung&uuml;ltige Aufstellung";
+
+            }
+
+        } else {
+
+            echo "Ung&uuml;ltige Aufstellung";
+
+        }
+
+    }
 	
 }

f47a09b606c6b25324dad662edae0c2bc6100e1f
Salvatore Mulas <salvo@salvoblog.ch>
2013-09-30 15:59:09 +0200

    Preparation for admin login module

--- application/controllers/site.php
+++ application/controllers/site.php
@@ -39,5 +39,59 @@ class Site extends MY_Controller {
         $hello = $this->ranking->getTables($teams);
         var_dump($hello);
     }
+    
+    public function login_validation() {
+
+        $this->form_validation->set_rules('username', 'Username', 'required|trim|xss_clean|callback_validate_credentials');
+        $this->form_validation->set_rules('password', 'Password', 'required|sha1');
+        
+        if ($this->form_validation->run()) {
+            
+            $this->load->model('model_users');
+            $username = $this->input->post('username');
+            $userid = $this->model_users->get_id_for_username($username);
+
+            $data = array(
+                'username' => $username,
+                'userid' => $userid,
+                'is_logged_in' => 1,
+            );
+            $this->session->set_userdata($data);
+            redirect('dashboard');
+        } else {
+            $this->index();
+        }
+    }
+    
+    public function validate_credentials() {
+        $this->load->model('model_users');
+
+        if ($this->model_users->can_log_in()) {
+            return true;
+        } else {
+            $this->form_validation->set_message('validate_credentials', 'Benutzername/Passwort ung&uuml;ltig.');
+            return false;
+        }
+    }
+    
+    public function logout() {
+        $this->session->sess_destroy();
+        redirect('/');
+    }
+    
+    public function restricted() {
+        $body_data['error_messages'][] = 'Sie sind nicht berechtigt, diese Seite aufzurufen.';
+        if (!$this->session->userdata('is_logged_in')) {
+            $body_data['error_messages'][] = 'Bitte melden Sie sich an und versuchen nochmals, die Seite zu laden.';
+        }
+        
+        $meta_data['page_title'] = 'Zugriff verweigert';
+        $body_data['page_title'] = $meta_data['page_title'];
+
+        // Show success/error message(s)
+        $this->load->view('header');
+        $this->load->view('meta', $meta_data);
+        $this->load->view('show_message', $body_data);
+    }
 
 }

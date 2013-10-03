cf3dbb7ce4f6035b92d1d007db94a5304b3050da
Salvatore Mulas <salvo@salvoblog.ch>
2013-10-03 18:12:51 +0200

    Commenting the code

--- application/config/pagination.php
+++ application/config/pagination.php
@@ -1,21 +1,34 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
  
+// Full tags
 $config['full_tag_open'] = '<div class="pagination"><ul>';
 $config['full_tag_close'] = '</ul></div>';
+
+// First tags and links
 $config['first_link'] = '&laquo; Erste';
 $config['first_tag_open'] = '<li class="prev page">';
 $config['first_tag_close'] = '</li>';
+
+// Last tags and links
 $config['last_link'] = 'Letzte &raquo;';
 $config['last_tag_open'] = '<li class="next page">';
 $config['last_tag_close'] = '</li>';
-$config['next_link'] = 'N&auml;chste &rarr;';
-$config['next_tag_open'] = '<li class="next page">';
-$config['next_tag_close'] = '</li>';
+
+// Previous tags and links
 $config['prev_link'] = '&larr; Vorherige';
 $config['prev_tag_open'] = '<li class="prev page">';
 $config['prev_tag_close'] = '</li>';
+
+// Next tags and links
+$config['next_link'] = 'N&auml;chste &rarr;';
+$config['next_tag_open'] = '<li class="next page">';
+$config['next_tag_close'] = '</li>';
+
+// Current tags
 $config['cur_tag_open'] = '<li class="active"><a href="">';
 $config['cur_tag_close'] = '</a></li>';
+
+// Numerical tags
 $config['num_tag_open'] = '<li class="page">';
 $config['num_tag_close'] = '</li>';
  

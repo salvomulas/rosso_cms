a8745b4157267d002a013557671b5d3b4542ed83
Salvatore Mulas <salvo@salvoblog.ch>
2013-11-12 20:34:40 +0100

    Minor changes to the layout

--- application/views/elements/silvesterUnit.php
+++ application/views/elements/silvesterUnit.php
@@ -3,25 +3,35 @@
         <h2 class="text-center">Silvester 2013/14 mit uns feiern!</h2>
         <h4 class="text-center">Die legend&auml;re AC Rossoneri Silvesterparty findet auch dieses Jahr wieder statt!</h4>
 
-        <div class="row text-center">
+        <div class="row text-center hidden-phone">
             <div class="span4">
                 <img class="img-rounded" src="<?php echo base_url(); ?>assets/img/silv_05.jpg" style="max-width: 270px;">
             </div>
             <div class="span4">
-                <img class="img-rounded" src="<?php echo base_url(); ?>assets/img/silv_03.jpg" style="max-width: 270px;">
+                <img class="img-rounded" src="<?php echo base_url(); ?>assets/img/silv_10.jpg" style="max-width: 270px; height: 180.078px;">
             </div>
             <div class="span4">
                 <img class="img-rounded" src="<?php echo base_url(); ?>assets/img/silv_09.jpg" style="max-width: 270px;">
             </div>
         </div>
         
-        <div class="pagination pagination-centered">
+        <div class="pagination pagination-centered hidden-phone">
             <ul>
                 <li class="active"><a href="">Informationen</a></li>
-                <li><a href="">Speisekarte</a></li>
+                <li><a href="">Gala-Dinner</a></li>
                 <li><a href="">Unterhaltung</a></li>
+                <li><a href="">Preise und Reservation</a></li>
             </ul>
-        </div>   
+        </div>
+
+        <div class="well visible-phone">
+            <ul class="nav nav-tabs nav-stacked">
+                <li class="active"><a href="">Informationen</a></li>
+                <li><a href="">Gala-Dinner</a></li>
+                <li><a href="">Unterhaltung</a></li>
+                <li><a href="">Preise und Reservation</a></li>
+            </ul>
+        </div>
         
     </div>
 </div>
--- application/views/pages/silvester.php
+++ application/views/pages/silvester.php
@@ -1,3 +1,64 @@
 <div class="container">
-    <h2>Es ist schon wieder soweit...</h2>
+
+    <div class="row">
+
+        <div class="span4 pull-right">
+
+            <div class="well">
+
+                <h4>Flyer herunterladen</h4>
+                <hr>
+                <ul class="nav nav-list">
+                    <li><a href="<?php echo base_url(); ?>assets/files/ACR_Statuten_DE.pdf">Deutsch</a></li>
+                    <li><a href="<?php echo base_url(); ?>assets/files/ACR_Statuto_IT.pdf">Italienisch</a></li>
+                </ul>
+
+            </div>
+
+            <div class="well">
+
+                <h4>Eventinformationen</h4>
+                <hr>
+                <table class="table table-hover table-condensed">
+                    <tr>
+                        <th>Datum</th>
+                        <td>31. Dezember 2013</td>
+                    </tr>
+                    <tr>
+                        <th>T&uuml;r&ouml;ffnung</th>
+                        <td>18:30 Uhr</td>
+                    </tr>
+                    <tr>
+                        <th>Veranstaltungsort</th>
+                        <td>Mehrzweckhalle Stutz, Lausen</td>
+                    </tr>
+                </table>
+
+            </div>
+
+        </div>
+
+        <div class="span8">
+
+            <h2>Es ist schon wieder soweit...</h2>
+
+            <p>Es ist wieder soweit - das Ende des Jahres steht einmal mehr vor der T&uuml;r und damit n&auml;hert sich auch unser traditionelles Silvesterfest. Wie &uuml;blich, werden wir in der Mehrzweckhalle Stutz von Lausen
+                f&uuml;r beste Unterhaltung sorgen. Dieses Jahr wird die italienische Band "I falsi miti" für musikalisches Furore und ordentliche Stimmung sorgen - begleitet wird die Musik durch feines Essen und gutem Wein.<br /><br/>
+
+                Ein Event f&uuml;r Jung und Alt, ein Silvesterfest nach italienischer Tradition - das Event des Jahres für Jedermann. Wer schon das Vergn&uuml;gen hatte an einem AC Rossoneri Silvesterfest teilzunehmen wird es
+                sicherlich nicht verpassen, und diejenigen die noch nie noch teilgenommen haben laden wir herzlich dazu ein sich von unserem Programm verf&uuml;hren zu lassen.<br/><br/>
+
+                Auf dieser Seite finden Sie s&auml;mtliche Informationen zum Event, sollten dennoch Fragen offen stehen bleiben steht Ihnen unser Organisationskomitee jederzeit gerne zur Verf&uuml;gung.<br/><br/>
+
+                Wir freuen uns sehr auf Ihren Besuch und w&uuml;nschen jetzt schon einen guten Rutsch ins neue Jahr!<br/>
+
+        </div>
+
+
+
+    </div>
+
+
+
 </div>
+<hr>

2db85c8b21645f8719c7d92a30948f2cd18076cf
Salvatore Mulas <salvo@salvoblog.ch>
2014-02-06 16:53:53 +0100

    Completed integration of HTML parser for automatic ranking tables

--- application/controllers/sandbox.php
+++ application/controllers/sandbox.php
@@ -5,13 +5,58 @@ if (!defined('BASEPATH'))
 
 class Sandbox extends CI_Controller {
 
-	public function index() {
+    private $firstID = 'ctl01_ctl00_ctl00_sfvVereinRanglisten_ctl01_tbRangliste';
+    private $secondID = 'ctl01_ctl00_ctl00_sfvVereinRanglisten_ctl03_tbRangliste';
+    private $thirdID = 'ctl01_ctl00_ctl00_sfvVereinRanglisten_ctl05_tbRangliste';
+    private $seniorenID = 'ctl01_ctl00_ctl00_sfvVereinRanglisten_ctl07_tbRangliste';
+    private $frauenID = 'ctl01_ctl00_ctl00_sfvVereinRanglisten_ctl09_tbRangliste';
+    
+    private $ranking = array();
+    
+    public function __construct() {
+        parent::__construct();
+        require_once('system/libraries/DomParser.php');
+    }
+    
+    public function index() {
 
-        $test = array(35,109,57,69,79,99,103,139,93,101,96,17,100,33,67,59,120,112);
-
-        echo serialize($test);
-
+       $this->parseRanking($this->firstID);
 
     }
-	
+    
+    public function parseRanking($teamID) {
+        
+        $html = new dom_parser();
+        $html->load_file('http://www.football.ch/fvnws/Fussballverband-Nordwestschweiz/Vereine-FVNWS/Verein-FVNWS.aspx/v-508/a-rr/');
+        $loopCounter = -1;
+        
+        foreach($html->find('table[id='.$teamID.']') as $table) {
+            foreach($table->find('tr[class=nisRanRowSP]') as $element) {
+                array_push($this->ranking, array());
+                $loopCounter++;
+                foreach ($element->find('td') as $detail) {
+                    array_push($this->ranking[$loopCounter], $detail->plaintext);
+                }
+
+            }
+        }
+        
+        print_r($this->ranking);
+        
+        $count = sizeof($this->ranking);
+        echo $count;
+        
+
+        echo '<table>';
+            for ($i=0; $i<sizeof($this->ranking); $i++) {
+                echo '<tr>';
+                for ($s=0; $s<sizeof($this->ranking[$i]); $s++) {
+                    echo '<td>'.$this->ranking[$i][$s].'</td>';
+                }
+                echo '</tr>';
+            }
+        echo '</table>';
+
+    }
+
 }
--- application/views/elements/staffTable.php
+++ application/views/elements/staffTable.php
@@ -1,26 +1,49 @@
+<h4>Staff</h4>
+<hr>
+
 <?php
 
 if ($staff->num_rows > 0) {
 
+    $counter = 1;
+
     foreach ($staff->result() as $row) {
 
-        echo '<div class="row-fluid">';
-        echo '<div class="span2">';
-        if ($row->pictureURL != NULL) {
-            echo '<img class="img-rounded" src="'.base_url().'assets/img/staff'.$row->pictureURL.'" style="height: 70px;">';
-        } else {
-            echo '<img class="img-rounded" src="'.base_url().'assets/img/dummy_person.jpg" style="height: 60px;">';
+        if ($counter == 1 || is_int(($counter + 2) / 3)) {
+            echo '<div class="row">';
         }
-        echo '</div>';
-        echo '<div class="span10">';
-        echo '<h5>'.$row->function.'</h5>';
-        echo '<p>'.$row->lastName.' '.$row->firstName.'<h5>';
-        echo '</div>';
-        echo '</div>';
-        echo '<hr>';
 
+        echo '<div class="col-md-4 col-sm-6">';
+
+        echo '<div class="panel panel-success">';
+        echo '<div class="panel-body">';
+        echo '<div class="row">';
+        echo '<div class="col-md-4 col-sm-4 col-xs-4">';
+
+        if ($row->pictureURL != NULL) {
+            echo '<img class="img-rounded" src="' . base_url() . 'assets/img/staff/' . $row->pictureURL . '" style="width: 100%;">';
+        } else {
+            echo '<img class="img-rounded" src="' . base_url() . 'assets/img/dummy_person.jpg" style="width: 100%;">';
+        }
+
+        echo '</div>';
+
+        echo '<div class="col-md-8 col-sm-8 col-xs-8">';
+        echo '<h5 class="trikot">' . $row->function . '</h5>';
+        echo '<h4>' . $row->firstName . ' ' . $row->lastName . '</h4>';
+
+        echo '</div>';
+
+
+        if (is_int($counter / 3) || $counter == $staff->num_rows()) {
+            echo '</div>';
+        }
+
+        echo '</div>';
+        echo '</div>';
+        echo '</div>';
+        echo '</div>';
+
+        $counter++;
     }
-    
-    } else {
-        echo '<p>Wird noch zugeteilt</p>';
-    }
+} 
